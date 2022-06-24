8cc58d36ef4c1e6376f5a14cafc25150f4649ecc
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-06-24 03:02:24 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-//Made by: Aiden McLeod
-//Made in: March 2022
+// Aiden McLeod
+// ICS2O-Assignment5-HTML
+// April 25 2022
 
 'use strict'
 /**
- * This function calculates area of a sphere.
+ * Check service worker.
  */
-function calculate() {
-  // input
-  const radius = parseInt(document.getElementById('radius').value)
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Assignment5-HTML/sw.js", {
+    scope: "/ICS2O-Assignment5-HTML/",
+  })
+}
 
-  // process
-
-  const volume = (4/3)* Math.PI * Math.pow(radius, 3);
-
-  // output
-  document.getElementById('volume').innerHTML = 'Volume is: ' + volume.toFixed(2) + ' cm³'
+/**
+ * Finds the value of pi using the user's number of iterations.
+ */
+function onButtonClick() {
+  var numberOfIterations = document.getElementById("number-of-iterations").value
+  var currentAction = "minus"
+  var number = 1
+  var pi
+  
+  pi = (4 / number)
+  while (numberOfIterations > 1) {
+    if (currentAction == "minus") {
+      number = number + 2
+      pi = pi - (4 / number)
+      currentAction = "plus"
+    } else if (currentAction == "plus") {
+      number = number + 2
+      pi = pi + (4 / number)
+      currentAction = "minus"
+    }
+    numberOfIterations = numberOfIterations - 1
+  }
+  currentAction = "minus"
+  number = 1
+  document.getElementById("result").innerHTML = "<p>π is equal to " + pi + "</p>"
 }
